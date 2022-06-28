0f19ea8d64c664c2d2ee2ef460404bbf9afaff7b
Artem Lukin <artem.lukin@valeo.com>
2022-06-28 10:20:51 +0200

    Add function to process new todo form callback

--- src/App.js
+++ src/App.js
@@ -1,17 +1,53 @@
 import "./App.css";
 import TodoList from "./components/TodoList";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import AddForm from "./components/AddForm";
+import { v4 as uuidv4 } from "uuid";
+import { base32hex } from "rfc4648";
 
 function App() {
+  const id = uuidv4();
+  const base32 = base32hex.stringify(id).slice(0, -6).toLowerCase();
   const testItem = {
     title: "test",
     description: "test description",
     dueDate: "2022-07-01",
+    id,
+    base32,
   };
-  const [todos, setTodos] = useState([testItem]);
+  const [todos, setTodos] = useState([]);
+  const [showForm, setShowForm] = useState(false);
+
+  const addTodo = (todo) => {
+    setTodos([...todos, todo]);
+  };
+
+  const formCallback = (e) => {
+    e.preventDefault();
+    const title = e.target[0].value;
+    const description = e.target[1].value;
+    const dueDate = e.target[2].value;
+    const id = uuidv4();
+    const base32 = base32hex.stringify(id).slice(0, -6).toLowerCase();
+
+    addTodo({ title, description, dueDate, id, base32 });
+  };
+
+  useEffect(() => {
+    addTodo(testItem);
+  }, []);
+
   return (
     <div className="App">
       <TodoList todos={todos} />
+      <button
+        onClick={() => {
+          setShowForm(true);
+        }}
+      >
+        Add Todo
+      </button>
+      {showForm && <AddForm onSubmit={formCallback} />}
     </div>
   );
 }
