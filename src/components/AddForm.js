79b99f1975566b267ede0182b7c3e7f2772e5fa6
Artem Lukin <artem.lukin@valeo.com>
2022-06-28 09:43:25 +0200

    Add form for new todos

---
+++ src/components/AddForm.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const AddForm = ({ onSubmit }) => {
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <label htmlFor="title">Title</label>
+        <input type="text" name="title" id="title" />
+        <label htmlFor="description">Description</label>
+        <input type="text" name="description" id="description" />
+        <label htmlFor="due-date">Due Date</label>
+        <input type="text" name="due-date" id="due-date" />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default AddForm;
