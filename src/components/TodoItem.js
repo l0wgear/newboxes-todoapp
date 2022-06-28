36a3d72019b96de521ef3d197112d53607293396
Artem Lukin <artem.lukin@valeo.com>
2022-06-28 16:49:35 +0200

    Add date formatting

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -5,7 +5,13 @@ const TodoItem = ({ item, onBtnClick }) => {
   return (
     <div className="rounded-lg p-4 dark:bg-slate-800 flex flex-col gap-3 h-fit w-fit">
       <h1 className="font-bold text-2xl capitalize">{title}</h1>
-      <span className="due-date">{dueDate}</span>
+      <span className="due-date">
+        {new Date(dueDate).toLocaleDateString("de-DE", {
+          day: "2-digit",
+          month: "2-digit",
+          year: "numeric",
+        })}
+      </span>
       <p>{description}</p>
       <button
         className="p-1 dark:bg-sky-600 justify-self-end rounded hover:dark:bg-sky-500 active:dark:bg-sky-700"
