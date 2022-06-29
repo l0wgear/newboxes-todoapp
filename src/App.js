953c0c0028c835d1fb7bed792cae84b6ca7f5e2c
Artem Lukin <artem.lukin@valeo.com>
2022-06-29 13:30:28 +0200

    Add google login button

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import "./App.css";
 import TodoList from "./components/TodoList";
 import React, { useState, useEffect } from "react";
 import AddForm from "./components/AddForm";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import { GoogleLogin } from "@react-oauth/google";
 
 function App() {
   const [todos, setTodos] = useState(
@@ -20,33 +22,43 @@ function App() {
   const markDone = (id) => {
     const confirmRes = window.confirm("Mark todo as done and remove?");
     if (confirmRes) {
-      const updatedTodos = todos.filter((item) => item.id != id);
+      const updatedTodos = todos.filter((item) => item.id !== id);
       setTodos(updatedTodos);
     }
   };
 
   return (
-    <div className="App relative flex flex-col antialiased text-slate-700 dark:text-slate-200 bg-white dark:bg-slate-900 w-screen h-screen">
-      <header className="min-h-6 w-screen py-5 px-3 bg-slate-800">
-        <button
-          className="p-3 dark:bg-sky-600 rounded hover:dark:bg-sky-500 active:dark:bg-sky-700"
-          onClick={() => {
-            setShowForm(true);
-          }}
-        >
-          Add Todo
-        </button>
-      </header>
-      <TodoList todos={todos} onBtnClick={markDone} />
-      {showForm && (
-        <AddForm
-          onClose={() => {
-            setShowForm(false);
-          }}
-          addTodo={addTodo}
-        />
-      )}
-    </div>
+    <GoogleOAuthProvider clientId="<your_client_id>">
+      <div className="App relative flex flex-col antialiased text-slate-700 dark:text-slate-200 bg-white dark:bg-slate-900 w-screen h-screen">
+        <header className="min-h-6 w-screen py-5 px-3 bg-slate-800 flex items-center justify-end gap-4">
+          <button
+            className="h-[40px] px-3 dark:bg-sky-600 rounded hover:dark:bg-sky-500 active:dark:bg-sky-700"
+            onClick={() => {
+              setShowForm(true);
+            }}
+          >
+            Add Todo
+          </button>
+          <GoogleLogin
+            onSuccess={(credentialResponse) => {
+              console.log(credentialResponse);
+            }}
+            onError={() => {
+              console.log("Login Failed");
+            }}
+          />
+        </header>
+        <TodoList todos={todos} onBtnClick={markDone} />
+        {showForm && (
+          <AddForm
+            onClose={() => {
+              setShowForm(false);
+            }}
+            addTodo={addTodo}
+          />
+        )}
+      </div>
+    </GoogleOAuthProvider>
   );
 }
 
