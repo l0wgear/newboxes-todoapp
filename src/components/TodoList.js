2cf09b103c9551f7b5bfbe0506b1291171251837
Artem Lukin <artem.lukin@valeo.com>
2022-06-29 13:49:28 +0200

    Add empty todo list placeholder

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,7 +2,11 @@ import React from "react";
 import TodoItem from "./TodoItem";
 
 const TodoList = ({ todos, onBtnClick }) => {
-  return (
+  return todos.length === 0 ? (
+    <div className="py-3 px-3 flex flex-wrap justify-center items-center gap-4 h-screen">
+      <p className="font-semibold text-2xl">No todos to show</p>
+    </div>
+  ) : (
     <div className="py-3 px-3 flex flex-wrap justify-start gap-4">
       {todos.map((item) => (
         <TodoItem item={item} key={item.id} onBtnClick={onBtnClick} />
