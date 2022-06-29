3dcf35198f1d47bbf70441c1ac5a57d32d32903b
Artem Lukin <artem.lukin@valeo.com>
2022-06-29 14:23:15 +0200

    Fix todo item layout

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -7,7 +7,7 @@ const TodoItem = ({ item, onBtnClick }) => {
   const today = new Date();
   const overdue = dueDateObject - today < 0;
   return (
-    <div className="rounded-lg p-4 bg-slate-300 dark:bg-slate-800 flex flex-col justify-between gap-3 h-fit w-fit min-w-[10rem] min-h-[11.2rem]">
+    <div className="rounded-lg p-4 bg-slate-300 dark:bg-slate-800 flex flex-col justify-between gap-3 h-fit w-fit min-w-[10rem] max-w-[15rem] min-h-[11.2rem]">
       <h1 className="font-bold text-2xl capitalize">{title}</h1>
       {dueDate !== "" && (
         <span className="due-date flex items-baseline">
@@ -19,7 +19,8 @@ const TodoItem = ({ item, onBtnClick }) => {
           {overdue && <Overdue />}
         </span>
       )}
-      <p>{description}</p>
+      <p className="flex-1">{description}</p>
+
       <button
         className="p-1 bg-sky-600 justify-self-end rounded hover:bg-sky-500 active:bg-sky-700 text-slate-200"
         onClick={() => {
