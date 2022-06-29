06aaeab731ac8af7ab536765d30506b586a50884
Artem Lukin <artem.lukin@valeo.com>
2022-06-29 13:52:44 +0200

    Fix invalid date error when no due date set

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -9,14 +9,16 @@ const TodoItem = ({ item, onBtnClick }) => {
   return (
     <div className="rounded-lg p-4 dark:bg-slate-800 flex flex-col gap-3 h-fit w-fit">
       <h1 className="font-bold text-2xl capitalize">{title}</h1>
-      <span className="due-date flex items-baseline">
-        {dueDateObject.toLocaleDateString("de-DE", {
-          day: "2-digit",
-          month: "2-digit",
-          year: "numeric",
-        })}
-        {overdue && <Overdue />}
-      </span>
+      {dueDate !== "" && (
+        <span className="due-date flex items-baseline">
+          {dueDateObject.toLocaleDateString("de-DE", {
+            day: "2-digit",
+            month: "2-digit",
+            year: "numeric",
+          })}
+          {overdue && <Overdue />}
+        </span>
+      )}
       <p>{description}</p>
       <button
         className="p-1 dark:bg-sky-600 justify-self-end rounded hover:dark:bg-sky-500 active:dark:bg-sky-700"
