ff77ea9e5b1070c09abff31aa7a12c84eca358f2
Artem Lukin <artem.lukin@valeo.com>
2022-06-29 12:54:05 +0200

    Add overdue indication

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,16 +1,21 @@
 import React from "react";
+import Overdue from "./Overdue";
 
 const TodoItem = ({ item, onBtnClick }) => {
   const { title, description, dueDate, id } = item;
+  const dueDateObject = new Date(dueDate);
+  const today = new Date();
+  const overdue = dueDateObject - today < 0;
   return (
     <div className="rounded-lg p-4 dark:bg-slate-800 flex flex-col gap-3 h-fit w-fit">
       <h1 className="font-bold text-2xl capitalize">{title}</h1>
-      <span className="due-date">
-        {new Date(dueDate).toLocaleDateString("de-DE", {
+      <span className="due-date flex items-baseline">
+        {dueDateObject.toLocaleDateString("de-DE", {
           day: "2-digit",
           month: "2-digit",
           year: "numeric",
         })}
+        {overdue && <Overdue />}
       </span>
       <p>{description}</p>
       <button
