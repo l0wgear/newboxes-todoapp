13ff311187688503c6d06b5796b9a6e3cde1adf0
Artem Lukin <artem.lukin@valeo.com>
2022-07-04 14:50:20 +0200

    Change todo layout

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,10 +2,25 @@ import React from "react";
 import Overdue from "./Overdue";
 
 const TodoItem = ({ item, onBtnClick }) => {
-  const { title, description, dueDate, id } = item;
+  const { title, description, dueDate, id, priority, status } = item;
   const dueDateObject = new Date(dueDate);
   const today = new Date();
   const overdue = dueDateObject - today < 0;
+  let bgPriority = "";
+  // switch (priority) {
+  //   case "high":
+  //     bgPriority = "bg-rose-500";
+  //     break;
+  //   case "medium":
+  //     bgPriority = "bg-yellow-500";
+  //     break;
+  //   case "low":
+  //     bgPriority = "bg-green-500";
+  //     break;
+  //   default:
+  //     bgPriority = "";
+  //     break;
+  // }
   return (
     <div className="rounded-lg p-4 bg-slate-300 dark:bg-slate-800 flex flex-col justify-between gap-3 h-fit w-fit min-w-[10rem] max-w-[15rem] min-h-[11.2rem]">
       <h1 className="font-bold text-2xl capitalize">{title}</h1>
@@ -19,16 +34,34 @@ const TodoItem = ({ item, onBtnClick }) => {
           {overdue && <Overdue />}
         </span>
       )}
+      <div className="flex items-center justify-between gap-1">
+        <span
+          className={`capitalize rounded p-1 text-slate-200 underline-offset-2 `}
+        >
+          {priority} priority
+        </span>
+        <span>|</span>
+        <span
+          className={`capitalize rounded p-1 text-slate-200 underline-offset-2 `}
+        >
+          {status}
+        </span>
+      </div>
       <p className="flex-1">{description}</p>
 
-      <button
-        className="p-1 bg-sky-600 justify-self-end rounded hover:bg-sky-500 active:bg-sky-700 text-slate-200"
-        onClick={() => {
-          onBtnClick(id);
-        }}
-      >
-        done
-      </button>
+      <div className="grid grid-rows-1 grid-cols-2 gap-1">
+        <button
+          className="p-1 bg-rose-700 justify-self-end rounded hover:bg-rose-600 active:bg-rose-800 text-slate-200 w-full"
+          onClick={() => {
+            onBtnClick(id);
+          }}
+        >
+          remove
+        </button>
+        <button className="p-1 bg-sky-600 justify-self-end rounded hover:bg-sky-500 active:bg-sky-700 text-slate-200 w-full">
+          edit
+        </button>
+      </div>
     </div>
   );
 };
