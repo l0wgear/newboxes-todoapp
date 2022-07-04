67348d596c9715930ef51d945400213423101fdf
Artem Lukin <artem.lukin@valeo.com>
2022-07-04 16:42:38 +0200

    Fix property name

--- src/components/Overdue.js
+++ src/components/Overdue.js
@@ -13,11 +13,11 @@ const Overdue = () => {
       >
         <path d="M256 512C114.6 512 0 397.4 0 256C0 114.6 114.6 0 256 0C397.4 0 512 114.6 512 256C512 397.4 397.4 512 256 512zM232 256C232 264 236 271.5 242.7 275.1L338.7 339.1C349.7 347.3 364.6 344.3 371.1 333.3C379.3 322.3 376.3 307.4 365.3 300L280 243.2V120C280 106.7 269.3 96 255.1 96C242.7 96 231.1 106.7 231.1 120L232 256z" />
       </svg>
-      <div class="absolute bottom-0 flex-col items-center hidden mb-6 group-hover:flex">
-        <span class="relative z-10 p-2 text-s leading-none text-white whitespace-no-wrap bg-slate-600 shadow-lg rounded">
+      <div className="absolute bottom-0 flex-col items-center hidden mb-6 group-hover:flex">
+        <span className="relative z-10 p-2 text-s leading-none text-white whitespace-no-wrap bg-slate-600 shadow-lg rounded">
           Overdue
         </span>
-        <div class="w-3 h-3 -mt-2 rotate-45 bg-slate-600"></div>
+        <div className="w-3 h-3 -mt-2 rotate-45 bg-slate-600"></div>
       </div>
     </div>
   );
